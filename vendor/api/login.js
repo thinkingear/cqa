8ff83dc23565d5ee9568958b5322767773675391
chiweigear <1987714707@qq.com>
2023-04-02 15:05:45 +0800

    init vendor(axios)

---
+++ vendor/api/login.js
@@ -0,0 +1,14 @@
+import request from '/request'
+
+/*
+export function login(account, password) {
+  const data = {
+    account,
+    password
+  }
+  return request({
+    url: '/login',
+    method: 'post',
+    data
+  })
+}*/
